a4582a6e71861d0bfc1e2925e7f6fde81a78763f
McElectrik <123sbv@mail.ru>
2020-11-19 08:41:47 +0300

    @costudiolabor
    Перетащил загатовки скриптов мины. Ничего ни успел.

---
+++ Team20Project_URP/Assets/Scripts/CreateMine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMine : MonoBehaviour
+{
+
+    [SerializeField] private GameObject MinePrefab = null;          // Префаб мины
+    [SerializeField] private int MaxCountMine = 5;                  // Кол-во мин на игру
+    private 
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.S) && (gameObject.tag == "CreatorMines1"))
+        {
+
+            InstansMine();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && (gameObject.tag == "CreatorMines2"))
+        {
+
+            InstansMine();
+
+
+
+        }
+
+
+    }
+    /// <summary>
+    /// Создание мины
+    /// </summary>
+
+    private void InstansMine() 
+    {
+        Instantiate(MinePrefab, transform.position, Quaternion.identity);  // создание мины 
+
+        Debug.Log("Mine");
+    }
+}
---
+++ Team20Project_URP/Assets/Scripts/DestroyMine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyMine : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Player2")
+        {
+            Debug.Log(other.tag);
+            Destroy(gameObject);
+        }
+    }
+}
